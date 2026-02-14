0a1db1e3e7c2c7233e69bfbb4ee93d7ca5fba879
Emmanuel Fauzer <fauzer@gmail.com>
2026-02-14 19:23:03 -0300

    Commit inicial do projeto de desafio de POO da DIO.

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import br.com.inovasoftware.desafio.dominio.Curso;
+import br.com.inovasoftware.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de Java");
+        curso1.setDescricao("Curso de Java da DIO.");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso de JavaScript");
+        curso2.setDescricao("Curso de JavaScript da DIO.");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Mentoria de Java do Bootcamp da Dio.");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
